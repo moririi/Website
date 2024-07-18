d49aeda078003703bdc61a43b3eb05ab3b183983
unknown <headshoteveryday10@gmail.com>
2024-07-18 23:28:24 +0800

    commented out header in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
 import "./App.css";
 import { useState, useEffect } from "react";
 import Loading from "./components/Header/Loading";
